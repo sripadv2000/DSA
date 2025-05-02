5c7ed260cb71407ddd2a62d5c487810fe52a438e
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-05-02 18:37:07 -0500

    Time: 3 ms (63.62%) | Memory: 17.8 MB (80.33%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -1,15 +1,15 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         hash_map = Counter(nums)
-        max_heap = []
+        min_heap = []
         
         # Push (frequency, -value) to sort by increasing frequency and decreasing value
         for num in hash_map:
-            heapq.heappush(max_heap, (hash_map[num], -num))
+            heapq.heappush(min_heap, (hash_map[num], -num))
         
         res = []
-        while max_heap:
-            f, neg_val = heapq.heappop(max_heap)
-            res.extend([-neg_val] * f)
+        while min_heap:
+            f, val = heapq.heappop(min_heap)
+            res.extend([-val] * f)
         return res
         

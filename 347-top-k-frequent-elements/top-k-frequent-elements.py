82570fa3329c62cf3eb11be5feed559b6774c969
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-05-02 17:10:24 -0500

    Time: 6 ms (55.50%) | Memory: 21.4 MB (40.18%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashMap = defaultdict(int)
+        freq_map = Counter(nums)
         min_heap = []
 
-        for num in nums:
-            hashMap[num] += 1
-
-        for key, val in hashMap.items():
-            heapq.heappush(min_heap, (val, key))
+        for num, freq in freq_map.items():
+            heapq.heappush(min_heap, (freq, num))
             if len(min_heap) > k:
                 heapq.heappop(min_heap)
 
-        res = []
-        for key, val in min_heap:
-            res.append(val)
-        return res
+        return [num for freq, num in min_heap]

02784472979118d201abee79332f41a297dab043
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-05-04 02:01:56 -0500

    Time: 7 ms (92.01%) | Memory: 24.3 MB (81.31%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
@@ -2,14 +2,10 @@ class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
         num_of_pairs = 0
         store_dominoes = defaultdict(int)
-        n = len(dominoes)
 
-        for i in range(0, n):
-            a, b = dominoes[i][0], dominoes[i][1]
+        for a,b in dominoes:
             if a > b:
-                a, b = dominoes[i][1], dominoes[i][0]
+                a, b = b,a
+            num_of_pairs += store_dominoes[(a,b)]
             store_dominoes[(a,b)] += 1
-                
-        for num in store_dominoes.values():
-            num_of_pairs += num*(num-1) // 2
         return num_of_pairs

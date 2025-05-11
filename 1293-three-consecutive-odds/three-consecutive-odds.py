910152adb88f23eaf607ac3d350fe07fa1979ce7
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-05-11 13:30:45 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (18.18%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.py
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -1,9 +1,11 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        N = len(arr)
-        if N < 3:
-            return False
-        for i in range(1, len(arr)-1):
-            if (arr[i-1] % 2) and (arr[i] % 2) and (arr[i+1] % 2):
+        streak = 0
+        for num in arr:
+            if num % 2 == 1:
+                streak += 1
+            else:
+                streak = 0
+            if streak == 3:
                 return True
         return False

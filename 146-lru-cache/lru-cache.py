03b3995b81bbc9c1ba1761b75089592211971364
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-05-14 18:37:37 -0500

    Time: 136 ms (61.56%) | Memory: 78.4 MB (55.65%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -6,43 +6,42 @@ class Node:
         self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}
+        self.capacity = capacity
+        self.map = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-        self.oldest = Node(0, 0)
-        self.latest = Node(0, 0)
-        self.oldest.next = self.latest
-        self.latest.prev = self.oldest
-        
+    def _remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
+        if key in self.map:
+            node = self.map[key]
+            self._remove(node)
+            self._add(node)
+            return node.val
         return -1
 
-    def remove(self, node):
-        prev, next = node.prev, node.next
-        prev.next = next
-        next.prev = prev
-    
-    def insert(self, node):
-        prev, next = self.latest.prev, self.latest
-        prev.next = next.prev = node
-        node.next = next
-        node.prev = prev
-
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        if key in self.map:
+            self._remove(self.map[key])
+        node = Node(key, value)
+        self._add(node)
+        self.map[key] = node
 
-        if len(self.cache) > self.cap:
-            lru = self.oldest.next
-            self.remove(lru)
-            del self.cache[lru.key]
-        
+        if len(self.map) > self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.map[lru.key]

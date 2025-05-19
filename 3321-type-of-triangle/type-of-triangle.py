19a1da9060b239be207276d7174f17a712cf4bcc
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-05-18 19:24:30 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (51.84%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.py
+++ 3321-type-of-triangle/type-of-triangle.py
@@ -4,7 +4,7 @@ class Solution:
         a, b, c = nums[0], nums[1], nums[2]
         if (a+b) <= c:
             return "none"
-        if a==b and b==c:
+        if a==c:
             return "equilateral"
         elif a==b or b==c:
             return "isosceles"

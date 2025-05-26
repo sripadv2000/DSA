3526719d311139b3a88642b70105f3e9de92dc38
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-05-26 01:10:06 -0500

    Time: 3 ms (96.06%) | Memory: 18.6 MB (94.39%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,12 +1,30 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        sorted_version = sorted(nums)
-        start, end = 0, len(nums)-1
-        while start < len(nums) and nums[start] == sorted_version[start]:
-            start += 1
-        while end > 0 and nums[end] == sorted_version[end]:
-            end -= 1
-        if end - start >= 0:
-            return end - start + 1
-        else:
+        N = len(nums)
+        low, high = 0, N-1
+
+        # 1. Find the first index violating ascending order
+        while low < N-1 and nums[low] <= nums[low + 1]:
+            low += 1
+        
+        # 2. Find the first index violating descending order
+        while high > 0 and nums[high - 1] <= nums[high]:
+            high -= 1
+        
+        # If low >= high means that the array is already sorted.
+        if low >= high:
             return 0
+        
+        # 3. Find min and max of our temporary window nums[lo: hi + 1]
+        windowMax = max(nums[low:high+1])
+        windowMin = min(nums[low:high+1])
+
+        # 4. Extend the window to left side if it violates windowMin
+        while low > 0 and nums[low - 1] > windowMin:
+            low -= 1
+        
+        # 5. Extend the window to right side if it violates windowMax
+        while high < N-1 and nums[high + 1] < windowMax:
+            high += 1
+        
+        return high - low + 1

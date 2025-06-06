ef4db59b3dc914c691164ddce1ba8ffbf8b8658e
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-06-05 20:29:58 -0500

    Time: 1155 ms (18.32%) | Memory: 21.3 MB (32.82%) - LeetSync

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        cnt, t, p = Counter(s), [], []
+
+        for ch in s:
+            t.append(ch)
+            
+            if cnt[ch] == 1:
+                del cnt[ch]
+            else:
+                cnt[ch] -= 1
+            
+            while cnt and t and min(cnt) >= t[-1]:
+                p += t.pop()
+        p += t[::-1]
+                
+        return "".join(p)

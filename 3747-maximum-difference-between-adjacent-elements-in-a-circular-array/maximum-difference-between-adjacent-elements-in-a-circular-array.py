11425aadc1f3effc500a8acb11328b247373fe51
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-06-11 19:14:45 -0500

    Time: 3 ms (53.96%) | Memory: 17.9 MB (29.16%) - LeetSync

--- 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.py
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.py
@@ -3,6 +3,5 @@ class Solution:
         length = len(nums)
         max_diff = abs(nums[0] - nums[length-1])
         for i in range(1, length):
-            if abs(nums[i] - nums[i-1]) > max_diff:
-                max_diff = abs(nums[i] - nums[i-1])
+            max_diff = max(max_diff, abs(nums[i] - nums[i-1]))
         return max_diff

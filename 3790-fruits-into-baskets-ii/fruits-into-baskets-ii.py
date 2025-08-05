01d0e40878627275edd75591bcce53543fd0ba8c
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-08-05 00:20:16 -0500

    Time: 19 ms (69.23%) | Memory: 17.8 MB (56.87%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         n = len(fruits)
-        s = set()
+        used = [False] * n
         cnt = 0
 
         for i in range(n):
             for j in range(n):
-                if fruits[i] <= baskets[j] and j not in s:
-                    s.add(j)
-                    cnt += 1
+                if not used[j] and fruits[i] <= baskets[j]:
+                    used[j] = True
                     break
+            else:
+                cnt += 1
 
-        return (n-cnt)
+        return cnt

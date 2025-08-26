d4f25b38c17ce2c36bb65345638a14a4873a7c76
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-08-25 23:03:52 -0500

    Time: 2 ms (44.27%) | Memory: 17.9 MB (41.49%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.py
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.py
@@ -2,8 +2,10 @@ class Solution:
     def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
         area, diagonal = 0, 0
         for i in range(len(dimensions)):
-            temp_diagonal = (dimensions[i][0])**2 + (dimensions[i][1])**2
-            if (temp_diagonal > diagonal) or (temp_diagonal == diagonal and dimensions[i][0] * dimensions[i][1] > area) :
+            length = dimensions[i][0]
+            breadth = dimensions[i][1]
+            temp_diagonal = length**2 + breadth**2
+            if (temp_diagonal > diagonal) or (temp_diagonal == diagonal and length * breadth > area) :
                 diagonal = temp_diagonal
-                area = dimensions[i][0] * dimensions[i][1]
+                area = length * breadth
         return area

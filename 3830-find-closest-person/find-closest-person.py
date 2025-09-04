366796b221113a4f16979dfcd3f540a4eecae696
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-09-03 23:10:20 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.11%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        dist1 = abs(x - z)
+        dist2 = abs(y - z)
+        if dist1 < dist2:
+            return 1
+        elif dist1 > dist2:
+            return 2
+        else:
+            return 0

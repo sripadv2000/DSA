9367ff6f6a68febbe5de66268becf3ceec10a676
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2025-09-08 12:52:06 -0500

    Time: 174 ms (20.72%) | Memory: 77.8 MB (91.98%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,26 +1,28 @@
 class Node:
+
     def __init__(self, key, val):
-        self.key = key
+        self.key  = key
         self.val = val
         self.prev = None
         self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.map = {}
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
         self.head.next = self.tail
         self.tail.prev = self.head
-
-    def _remove(self, node):
+    
+    def remove(self, node):
         prev = node.prev
         nxt = node.next
         prev.next = nxt
         nxt.prev = prev
-
-    def _add(self, node):
+    
+    def add(self, node):
         node.next = self.head.next
         node.prev = self.head
         self.head.next.prev = node
@@ -29,19 +31,25 @@ class LRUCache:
     def get(self, key: int) -> int:
         if key in self.map:
             node = self.map[key]
-            self._remove(node)
-            self._add(node)
+            self.remove(node)
+            self.add(node)
             return node.val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.map:
-            self._remove(self.map[key])
+            self.remove(self.map[key])
         node = Node(key, value)
-        self._add(node)
+        self.add(node)
         self.map[key] = node
 
         if len(self.map) > self.capacity:
             lru = self.tail.prev
-            self._remove(lru)
+            self.remove(lru)
             del self.map[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)

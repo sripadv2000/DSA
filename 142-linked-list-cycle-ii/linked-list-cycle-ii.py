43152d102f0171d33dc731de59903d9411c2fd50
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-04-04 18:07:03 -0500

    Time: 54 ms (48.44%) | Memory: 22.2 MB (97.35%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -6,17 +6,15 @@
 
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        if(head==None or head.next==None):
-            return None
         slow = head
         fast = head
-        entry = head 
-        while(fast.next!=None and fast.next.next!=None):
-            fast=fast.next.next
-            slow=slow.next
-            if(slow==fast):
-                while(slow!=entry):
-                    slow=slow.next
-                    entry=entry.next
-                return entry
-        return None #if fast reaches None and no cycle exists
+        while fast is not None and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None

b2d79f937f7476fab6d11f11ea217b646d52c8e9
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-04-11 01:51:16 -0500

    Time: 315 ms (74.33%) | Memory: 51.2 MB (14.44%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -1,24 +1,18 @@
 from collections import defaultdict
 
 class Solution:
-    def minimumDistance(self, nums):
-        mp = defaultdict(list)
+    def minimumDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        map = defaultdict(list)
 
-        for i, num in enumerate(nums):
-            mp[num].append(i)
+        for idx, num in enumerate(nums):
+            map[num].append(idx)
 
-        mini = float('inf')
+        mini = 2*n
 
-        for temp in mp.values():
+        for temp in map.values():
             m = len(temp)
-
             if m >= 3:
-                for i in range(m - 2):
-                    a = temp[i]
-                    b = temp[i + 1]
-                    c = temp[i + 2]
-
-                    diff = 2 * (max(a, b, c) - min(a, b, c))
-                    mini = min(mini, diff)
-
-        return -1 if mini == float('inf') else mini
+                for i in range(m-2):
+                    mini = min(2*(temp[i+2]-temp[i]), mini)
+        return -1 if mini == 2*n else mini

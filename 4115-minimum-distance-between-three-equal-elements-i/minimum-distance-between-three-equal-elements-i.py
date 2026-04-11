11e1592bbe8de388d281316fc6e015acc83fa409
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-04-10 23:19:44 -0500

    Time: 7 ms (39.81%) | Memory: 19.3 MB (76.70%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -1,13 +1,17 @@
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
-        res = sys.maxsize
         n = len(nums)
+        res = 2*n
         for i in range(n-2):
             for j in range(i+1, n-1):
+                if nums[i] != nums[j]:
+                    continue
                 for k in range(j+1, n):
-                    if nums[i]==nums[j]==nums[k]:
-                        res = min(abs(i-j)+abs(j-k)+abs(k-i), res)
-        if res != sys.maxsize:
+                    if nums[j]==nums[k]:
+                        res = min(2*(k-i), res)
+                        break
+
+        if res != 2*n:
             return res
         else:
             return -1

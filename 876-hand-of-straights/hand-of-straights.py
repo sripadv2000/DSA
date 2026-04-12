510309c224d201b9adcc9ebd632f36e1ec65bd65
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-04-11 23:11:42 -0500

    Time: 35 ms (45.48%) | Memory: 21.3 MB (31.09%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,35 +1,23 @@
+from collections import Counter
+import heapq
+
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        # If total cards can't be divided evenly, return False
         if len(hand) % groupSize != 0:
             return False
-
-        # Store the frequency of each card
+        
         freq = Counter(hand)
+        minheap = list(freq.keys())
+        heapq.heapify(minheap)
 
-        # Use a min heap to always get the smallest available card
-        minHeap = list(freq.keys())
-        heapq.heapify(minHeap)
+        while minheap:
+            first = minheap[0]
 
-        # Loop until all groups are formed
-        while minHeap:
-            # Start from the smallest card available
-            first = minHeap[0]
-
-            # Form a group starting from this card
             for i in range(groupSize):
                 card = first + i
-
-                # If card not in freq or frequency is 0, return False
                 if freq[card] == 0:
                     return False
-
-                # Decrease frequency
                 freq[card] -= 1
-
-                # If frequency is 0 and it's the top of the heap, pop it
-                if freq[card] == 0 and card == minHeap[0]:
-                    heapq.heappop(minHeap)
-
-        # All groups formed successfully
+                if freq[card] == 0 and card == minheap[0]:
+                    heapq.heappop(minheap)
         return True

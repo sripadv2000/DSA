f9cbaf131f5d2bc7a955e3c2f854ca400a8f0042
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-04-12 15:29:14 -0500

    Time: 199 ms (39.28%) | Memory: 34.8 MB (74.15%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,16 +1,27 @@
+from collections import deque
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        maxheap = []
+        n = len(nums)
+        # use deque
+        # keep adding elements to deque if number is smaller
+        # if you get any greater element, then keep popping
+        # if element removed from array is the left most in deque, pop from deque too.
         res = []
-        
-        for i in range(len(nums)):
-            heapq.heappush(maxheap, (-nums[i], i))
-            
-            # Lazy deletion: pop stale elements
-            while maxheap[0][1] < i - k + 1:
-                heapq.heappop(maxheap)
-            
-            if i >= k - 1:
-                res.append(-maxheap[0][0])
-        
+        q = deque()
+        l, r = 0, 0
+        while r < n:
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                res.append(nums[q[0]])
+                l += 1
+            r += 1
         return res
+        

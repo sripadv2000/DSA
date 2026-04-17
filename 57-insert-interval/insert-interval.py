2aa429ed7893e9b2340c32247e01b2a9e7f2ea1a
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-04-17 12:59:59 -0500

    Time: 4 ms (12.42%) | Memory: 21.4 MB (32.77%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,21 +1,27 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals:
+            return [newInterval]
+        
         n = len(intervals)
-        i = 0
-        res = []
+        target = newInterval[0]
+        left, right = 0, n-1
 
-        # Case 1: No overlapping before merging intervals
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
-        # Case 2: Overlapping and merging the interval
-        while i < n and newInterval[1] >= intervals[i][0]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-        res.append(newInterval)
-        # Case 3: No overlapping after merging newInterval
-        while i<n:
-            res.append(intervals[i])
-            i += 1
+        # Binary search to find the position to insert newInterval
+        while left <= right:
+            mid = (left+right)//2
+            if intervals[mid][0] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        intervals.insert(left, newInterval)
+
+        res = []
+        for interval in intervals:
+            if not res or res[-1][1]<interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
         return res
+        

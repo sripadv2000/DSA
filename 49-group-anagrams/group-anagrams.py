870b4ba0cf232041cbfd54442cbf5880b937bf4c
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-04-27 13:34:24 -0500

    Time: 11 ms (84.52%) | Memory: 22.6 MB (40.32%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-        res = defaultdict(list) # mapping charCount to list of Anagrams
-
-        for word in strs:
-            sorted_word = ''.join(sorted(word))
-                
-            res[sorted_word].append(word)
-
-        return list(res.values())
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return list(ans.values())

0c9a611dbdbef6d01c84ecf5a094d7369f7b2a23
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-04-28 00:51:05 -0500

    Time: 63 ms (19.00%) | Memory: 31.1 MB (27.41%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -2,13 +2,22 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         # 1 <= ANS <= n+1
         n = len(nums)
-        refer = [0]*(n+1)
-
-        for num in nums:
-            if 0 < num < (n+1):
-                refer[num] = 1
         
-        for i in range(1, n+1):
-            if not refer[i]:
-                return i
+        # Clean the array
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = (n+1)
+
+        # Mark the presence
+        for i in range(n):
+            num = abs(nums[i])
+            if num > n:
+                continue
+            if nums[num - 1] > 0:
+                nums[num - 1] = -nums[num - 1]
+        
+        # Find the missing positive
+        for i in range(n):
+            if nums[i] > 0:
+                return i+1
         return n+1

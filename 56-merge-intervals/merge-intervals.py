1d7a18d31871df7c536c354645bcb0d2e63c4406
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-05-09 15:22:20 -0500

    Time: 9 ms (33.90%) | Memory: 22.5 MB (70.22%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,10 +4,8 @@ class Solution:
         intervals.sort(key = lambda x : x[0])
 
         for interval in intervals:
-            if not merged:
-                merged.append(interval)
-            if merged[-1][1] < interval[0]:
+            if not merged or merged[-1][1] < interval[0]:
                 merged.append(interval)
             elif merged[-1][1] < interval[1]:
-                merged[-1][1] =  interval[1]
+                merged[-1][1] = interval[1]
         return merged

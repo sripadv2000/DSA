af7cca02185054c9383e257b839320d6d40b2852
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-05-19 08:27:14 -0500

    Time: 3 ms (98.60%) | Memory: 37.8 MB (51.94%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,22 +1,15 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        minheap1, minheap2 = [], []
-        
-        p1 = 0
-        while p1 < len(nums1):
-            heapq.heappush(minheap1, nums1[p1])
-            p1 += 1
-        p2 = 0
-        while p2 < len(nums2):
-            heapq.heappush(minheap2, nums2[p2])
-            p2 += 1
-        
-        while minheap1 and minheap2:
-            if minheap1[0] == minheap2[0]:
-                return minheap1[0]
-            elif minheap1[0] < minheap2[0]:
-                heapq.heappop(minheap1)
-            else:
-                heapq.heappop(minheap2)
-        
+        n1 = len(nums1)
+        n2 = len(nums2)
+
+        p1, p2 = 0, 0
+
+        while p1 < n1 and p2 < n2:
+            if nums1[p1] < nums2[p2]:
+                p1 += 1
+            elif nums1[p1] > nums2[p2]:
+                p2 += 1
+            elif nums1[p1] == nums2[p2]:
+                return nums1[p1]
         return -1

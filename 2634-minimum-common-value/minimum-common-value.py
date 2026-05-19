e80e0ebba8a976c1d18d3e044421f0fcc9e5dfe9
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-05-18 19:29:32 -0500

    Time: 7 ms (85.33%) | Memory: 37.6 MB (98.85%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1.sort()
+        nums2.sort()
+        n1 = len(nums1)
+        n2 = len(nums2)
+
+        p1, p2 = 0, 0
+
+        while p1 < n1 and p2 < n2:
+            if nums1[p1] < nums2[p2]:
+                p1 += 1
+            elif nums1[p1] > nums2[p2]:
+                p2 += 1
+            elif nums1[p1] == nums2[p2]:
+                return nums1[p1]
+        return -1

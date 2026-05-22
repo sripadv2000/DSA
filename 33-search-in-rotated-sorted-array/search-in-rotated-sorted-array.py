a31b77354844ca84e748684a577044481d52d5a4
sripadv2000 <65086547+sripadv2000@users.noreply.github.com>
2026-05-22 15:02:18 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (39.04%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,47 +1,25 @@
-from collections import Counter
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # 153. Find Minimum in Rotated Sorted Array
-        def findPivot(nums):
-            if len(nums) == 1:
-                return 0
-            left, right = 0, len(nums) - 1
-
-            if nums[right] > nums[left]:
-                return left
-
-            while left <= right:
-                mid = left + (right - left) // 2
-                
-                if nums[mid - 1] > nums[mid]:
-                    return mid
-                if nums[mid] > nums[mid + 1]:
-                    return mid + 1
-                if nums[mid] > nums[0]:
-                    left = mid + 1
-                if nums[mid] < nums[0]:
-                    right = mid - 1
-        
-        def binarySearch(l, r):
-            while l <= r:
-                mid = l + (r-l)//2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            return -1
-
         n = len(nums)
-        pivot = findPivot(nums)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = left + (right - left) // 2
 
-        # If array is not rotated, search full array
-        if pivot == 0:
-            return binarySearch(0, n - 1)
+            # Case 1: find target
+            if nums[mid] == target:
+                return mid
 
-        # Decide which sorted half to search
-        if nums[0] <= target <= nums[pivot - 1]:
-            return binarySearch(0, pivot - 1)
-        else:
-            return binarySearch(pivot, n - 1)
+            # Case 2: subarray on mid's left is sorted
+            elif nums[mid] >= nums[left]:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            # Case 3: subarray on mid's right is sorted.
+            else:
+                if target <= nums[right] and target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
